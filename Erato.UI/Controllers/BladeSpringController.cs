62d29466910afc29991e5f3d5745d62718db02fd
robertzml <robertzml@126.com>
2015-01-04 16:11:38 +0800

    Import metronic theme v3.6.1.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Erato.UI/Controllers/BladeSpringController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Erato.UI.Controllers
+{
+    public class BladeSpringController : Controller
+    {
+        // GET: BladeSpring
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
