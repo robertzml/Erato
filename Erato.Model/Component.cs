4ae17f101a36fb47b5dcbae59cd490b0033502b1
Robert <robertzml@126.com>
2015-01-28 22:23:36 +0800

    add Component class. add insert coil component in blade spring.
    Signed-off-by: Robert <robertzml@126.com>

---
+++ Erato.Model/Component.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Erato.Model
+{
+    /// <summary>
+    /// 部材类
+    /// </summary>
+    public class Component
+    {
+        /// <summary>
+        /// LOTNO
+        /// </summary>
+        [Display(Name = "LOTNO")]
+        [BsonElement("lotNo")]
+        public string LotNo { get; set; }
+
+        /// <summary>
+        /// 数量
+        /// </summary>
+        [Display(Name = "数量")]
+        [BsonElement("number")]
+        public int Numbert { get; set; }
+    }
+}
