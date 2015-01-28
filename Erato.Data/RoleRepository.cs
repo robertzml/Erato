f7f7cf70a3eb9a18d7a2c7b235596881a9a5e261
chinaxujia <190668340@qq.com>
2015-01-28 20:33:55 +0800

    x

---
+++ Erato.Data/RoleRepository.cs
@@ -0,0 +1,125 @@
+﻿using Erato.Model;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erato.Data
+{
+    /// <summary>
+    /// 角色 Repository
+    /// </summary>
+    public class RoleRepository
+    {
+        #region Field
+        /// <summary>
+        /// Repository对象
+        /// </summary>
+        private IMongoRepository<Role> repository;
+        #endregion //Field
+
+        #region Constructor
+        /// <summary>
+        /// 角色 Repository
+        /// </summary>
+        public RoleRepository()
+        {
+            this.repository = new MongoRepository<Role>(RheaServer.EratoDatabase);
+        }
+        #endregion //Constructor
+
+        #region Method
+        /// <summary>
+        /// 获取所有角色
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Role> Get()
+        {
+            return this.repository.AsEnumerable();
+        }
+
+        /// <summary>
+        /// 获取角色
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <returns></returns>
+        public Role Get(string id)
+        {
+            return this.repository.GetById(id);
+        }
+
+        /// <summary>
+        /// 添加角色
+        /// </summary>
+        /// <param name="data">角色数据</param>
+        /// <returns></returns>
+        public ErrorCode Create(Role data)
+        {
+            try
+            {
+                this.repository.Add(data);
+
+                return ErrorCode.Success;
+            }
+            catch (Exception)
+            {
+                return ErrorCode.Exception;
+            }
+        }
+
+        /// <summary>
+        /// 编辑角色
+        /// </summary>
+        /// <param name="data">角色数据</param>
+        /// <returns></returns>
+        public ErrorCode Update(Role data)
+        {
+            try
+            {
+                this.repository.Update(data);
+            }
+            catch (Exception)
+            {
+                return ErrorCode.Exception;
+            }
+
+            return ErrorCode.Success;
+        }
+
+        /// <summary>
+        /// 删除角色
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ErrorCode Delete(string id)
+        {
+            try
+            {
+                this.repository.Delete(id);
+            }
+            catch (Exception)
+            {
+                return ErrorCode.Exception;
+            }
+
+            return ErrorCode.Success;
+        }
+
+        public bool RoleNoExists(string id)
+        {
+            return this.repository.Exists(n => n.RoleNo == id);
+        }
+
+        public bool RoleNameExists(string name)
+        {
+            return this.repository.Exists(n => n.RoleName == name);
+        }
+        #endregion //Method
+
+
+    }
+}
---
+++ Erato.Model/Role.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Erato.Model
+{
+    /// <summary>
+    /// 线圈类
+    /// </summary>
+    [CollectionName("role")]
+    public class Role : MongoEntity
+    {
+        /// <summary>
+        /// 角色编号
+        /// </summary>
+        [Required]
+        [Display(Name = "角色编号")]
+        [BsonElement("roleNo")]
+        [StringLength(4, MinimumLength = 4, ErrorMessage = "输入的角色编号必须为4位！")]
+        public string RoleNo { get; set; }
+
+        /// <summary>
+        /// 角色名称
+        /// </summary>
+        [BsonElement("roleName")]
+        [Required]
+        [Display(Name = "角色名称")]
+        [StringLength(30, ErrorMessage = "输入的角色名称必须小于30位！")]
+        public string RoleName { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        [BsonElement("remarks")]
+        [Display(Name = "备注")]
+        [StringLength(30, ErrorMessage = "输入的备注必须小于30位！")]
+        public string Remarks { get; set; }
+
+        /// <summary>
+        /// 模块名
+        /// </summary>
+        [BsonElement("module")]
+        [Required]
+        [Display(Name = "模块名")]
+        [StringLength(30, ErrorMessage = "输入的备注必须小于30位！")]
+        public string Module { get; set; }
+
+        /// <summary>
+        /// 可见权限
+        /// </summary>
+        [BsonElement("selectRole")]
+        [Display(Name = "可见权限")]
+        [Required]
+        public bool SelectRole { get; set; }
+
+        /// <summary>
+        /// 新增权限
+        /// </summary>
+        [BsonElement("addRole")]
+        [Display(Name = "新增权限")]
+        [Required]
+        public bool AddRole { get; set; }
+
+        /// <summary>
+        /// 编辑权限
+        /// </summary>
+        [BsonElement("editRole")]
+        [Display(Name = "编辑权限")]
+        [Required]
+        public bool EditRole { get; set; }
+
+        /// <summary>
+        /// 删除权限
+        /// </summary>
+        [BsonElement("delRole")]
+        [Display(Name = "删除权限")]
+        [Required]
+        public bool DelRole { get; set; }
+
+        /// <summary>
+        /// 解锁权限
+        /// </summary>
+        [BsonElement("unLockRole")]
+        [Display(Name = "解锁权限")]
+        [Required]
+        public bool UnLockRole { get; set; }
+
+        /// <summary>
+        /// 录入时间
+        /// </summary>
+        [Display(Name = "录入时间")]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        [BsonElement("operationTime")]
+        public DateTime OperationTime { get; set; }
+
+        /// <summary>
+        /// 录入人工号
+        /// </summary>
+        [Display(Name = "录入人工号")]
+        [BsonElement("operator")]
+        public string Operator { get; set; }
+    }
+}
